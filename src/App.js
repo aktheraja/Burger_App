72d176b7c9179cb95795e2bf8a2eac1823119f9f
aktheraja <wale.akin2007@gmail.com>
2019-08-23 10:08:25 -0600

    Burger app started

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+        <layout>
+          <p>Test</p>
+        </layout>
+
+      </div>
+    );
+  }
+}
+
+export default App;
