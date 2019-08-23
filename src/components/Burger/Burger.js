0b558d813a6292ebaeb21980cafa2fa78191c526
aktheraja <wale.akin2007@gmail.com>
2019-08-23 17:35:21 -0600

    more components added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,11 +2,17 @@ import React from 'react';
 import classes from './Burger.css'
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 const burger = (props)=>{
+    //creating a array of lengths depending on the object, e.g cheese will have length two and salad length one. create a unique key
+    //with igKey(e.g cheese) plus index
+    const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_,index) =>{
+        return <BurgerIngredient key ={igKey + index} type={igKey} />
+    });
+    });
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type={"bread-top"} />
-            <BurgerIngredient type={"cheese"} />
-            <BurgerIngredient type={"meat"} />
+            {transformedIngredients}
             <BurgerIngredient type={"bread-bottom"} />
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,11 +2,19 @@ import React, {Component} from 'react';
 import Aux from "../../hoc/Auxi";
 import Burger from '../../components/Burger/Burger'
 class BurgerBuilder extends Component{
+    state ={
+        ingredients:{
+            salad:1,
+            bacon:1,
+            cheese:2,
+            meat:2
+        }
+    }
     render(){
         return (
             // wrapping element
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Aux>
         );
