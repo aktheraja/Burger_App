0548c32bc48435d5669eddafe40ee329e32e552b
aktheraja <wale.akin2007@gmail.com>
2019-08-27 09:29:55 -0600

    updated

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Aux from '../../hoc/Auxi';
-import classes from './Layout.css'
+import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 const layout = (props) => (
     <Aux>
-    <div> Toolbar, SideDrawer, Backdrop</div>
-    <main className={classes.Content}>
-    {props.children}
-    </main>
+    <Toolbar/>
+    <SideDrawer/>
+             <main className={classes.Content}>
+                    {props.children}
+             </main>
     </Aux>
 );
 export default layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import classes from './Toolbar.css';
+import Logo from '../../Logo/Logo';
+import NaviagtionItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props)=>{
     return (
-        <header>
-            <div>MENU</div>
-            <div>LOGO</div>
+        <header className={classes.Toolbar}>
+            <div >MENU</div>
+            <div >
+            <Logo />
+            </div>
             <nav>
-                ......
+                <NaviagtionItems />
             </nav>
         </header>
     );
